fe505d2e55cc8ef755b5a94d345f3908504eeb98
Arif <muhammadaidil6451@gmail.com>
2024-05-31 16:56:21 +0700

    first init

---
+++ tutorial-bg-dea/myscript.js
@@ -0,0 +1,9 @@
+const body = document.body;
+const h1 = document.createElement("h1");
+h1.textContent = "test";
+
+const namaSaya = document.createElement("p");
+namaSaya.innerHTML = "muhammad aidil arif";
+
+body.append(h1);
+body.append(namaSaya);
